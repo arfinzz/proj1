175b50b13a43efee2531f4ea4718ddedc63bab09
arfinzz <khanarfin2000@gmail.com>
2023-05-17 15:58:14 +0530

    made 2nd list green and all list font bold

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ headbox.style.borderBottom="solid 3px #000";
 let titles=document.querySelectorAll(".title");
 titles[1].style.fontWeight="bold";
 titles[1].style.color="green";
+
+let lists=document.querySelectorAll(".list-group-item");
+
+lists[2].style.backgroundColor="green";
+
+for(let i=0;i<lists.length;i++)
+{
+    lists[i].style.fontWeight="bold";
+}
