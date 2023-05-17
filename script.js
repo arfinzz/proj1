4653d12e9489606b790852cd2435a3b514051abd
arfinzz <khanarfin2000@gmail.com>
2023-05-17 15:47:48 +0530

    added script and few functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+let headbox=document.querySelector("#main-header");
+
+headbox.style.borderBottom="solid 3px #000";
+
+
+let titles=document.querySelectorAll(".title");
+titles[1].style.fontWeight="bold";
+titles[1].style.color="green";
